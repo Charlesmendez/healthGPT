66beaca606534c4250b58263779851069ab38648
Carlos Mendez <charlesmendez@hotmail.com>
2023-12-20 13:30:15 -0500

    Initial Commit

---
+++ healthGPT/healthGPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  healthGPTApp.swift
+//  healthGPT
+//
+//  Created by Carlos Fernando Mendez Solano on 12/20/23.
+//
+
+import SwiftUI
+
+@main
+struct healthGPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
